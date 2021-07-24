6258e7128187b3f76a52973b601c49c1305f3d1c
RaphaelS1 <raphaelsonabend@gmail.com>
2021-07-24 16:21:02 +0100

    export expectation

---
+++ R/tools.R
@@ -0,0 +1,16 @@
+#' Check if two parameters are equal
+#' @description Primarily for internal use
+#' @param obj,expected [ParameterSet]
+#' @export
+expect_equal_ps <- function(obj, expected) {
+  pobj <- get_private(obj)
+  pexp <- get_private(expected)
+  expect_equal(sort(names(pobj)), sort(names(pexp)))
+  lapply(names(pexp), function(x) {
+    if (is.function(pexp[[x]])) {
+      expect_equal(deparse(pobj[[x]]), deparse(pexp[[x]]), info = x)
+    } else {
+      expect_equal(pobj[[x]], pexp[[x]], info = x)
+    }
+  })
+}
--- tests/testthat/helpers.R
+++ tests/testthat/helpers.R
@@ -1,17 +1,3 @@
-expect_equal_ps <- function(obj, expected) {
-  pobj <- get_private(obj)
-  pexp <- get_private(expected)
-  expect_equal(sort(names(pobj)), sort(names(pexp)))
-  lapply(names(pexp), function(x) {
-    if (is.function(pexp[[x]])) {
-      expect_equal(deparse(pobj[[x]]), deparse(pexp[[x]]), info = x)
-    } else {
-      expect_equal(pobj[[x]], pexp[[x]], info = x)
-    }
-  })
-}
-
-
 expect_R6_class <- function(obj, what) { # nolint
   expect_true(inherits(obj, c(what, "R6")))
 }
